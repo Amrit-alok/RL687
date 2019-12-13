c9b442d38e6b39b4df85b168432d9559644733e0
Amrit Alok <amrit.alok@hotmail.com>
2019-12-13 00:56:39 -0500

    Initial base

--- OptimizeJ.py
+++ OptimizeJ.py
@@ -4,34 +4,36 @@ from es import CMAES
 
 
 def problem4():
-    data_parition = 120000
+    data_parition = 200000
     pdis = PDIS(behavior_file="data.csv", data_partition=data_parition)
-    NPARAMS = 4  # make this a 100-dimensinal problem.
-    NPOPULATION = 100  # use population size of 101.
-    MAX_ITERATION = 5
-    cmaes = CMAES(NPARAMS,
-                  popsize=NPOPULATION,
-                  weight_decay=0.0,
-                  sigma_init=5.0
-                  )
+    pdis.calculate_pi_b()
 
-    history = []
-    print("candidate_data: ",data_parition,"Population: ",NPOPULATION, "Max Iteration: ",MAX_ITERATION)
-    for j in range(MAX_ITERATION):
-        solutions = cmaes.ask()
-        fitness_list = np.zeros(cmaes.popsize)
-        for i in range(cmaes.popsize):
-            fitness_list[i] = pdis.upper_bound(solutions[i])
-        cmaes.tell(fitness_list)
-        result = cmaes.result()  # first element is the best solution, second element is the best fitness
-        history.append(result[1])
-        if (j + 1) % 100 == 0:
-            print("fitness at iteration", (j + 1), result[1])
-    print("local optimum discovered by solver:\n", result[0])
-    print("fitness score at this local optimum:", result[1])
-    # return history
-    pdis.execute_safety_test(result[0])
+    for trials in range(10):
+        NPARAMS = 4  # make this a 100-dimensinal problem.
+        NPOPULATION = 70  # use population size of 101.
+        MAX_ITERATION = 10
+        cmaes = CMAES(NPARAMS,
+                      popsize=NPOPULATION,
+                      weight_decay=0.0,
+                      sigma_init=2.0
+                      )
 
+        history = []
+        print("candidate_data: ",data_parition,"Population: ",NPOPULATION, "Max Iteration: ",MAX_ITERATION)
+        for j in range(MAX_ITERATION):
+            solutions = cmaes.ask()
+            fitness_list = np.zeros(cmaes.popsize)
+            for i in range(cmaes.popsize):
+                fitness_list[i] = pdis.upper_bound(solutions[i])
+            cmaes.tell(fitness_list)
+            result = cmaes.result()  # first element is the best solution, second element is the best fitness
+            history.append(result[1])
+            if (j + 1) % 2 == 0:
+                print("fitness at iteration", (j + 1), result[1])
+        print("local optimum discovered by solver:\n", result[0])
+        print("fitness score at this local optimum:", result[1])
+        # return history
+        pdis.execute_safety_test(result[0],trials)
 
 
 problem4()
--- PDIS.py
+++ PDIS.py
@@ -6,138 +6,145 @@ from scipy import stats
 
 class PDIS:
 
+    @property
+    def pi_b(self):
+        return self._pi_b
+
     def __init__(self, behavior_file, data_partition):
-        self.data_partition = data_partition
+        self.data_partition = int(data_partition * 0.6)
+        print("candidate_data", self.data_partition)
         self.behavior_data = ReadBehaviorFile(behavior_file)
-        # print("theta_b: ", self.behavior_data.theta_b)
-        # print("Policy representation from file: ", self.behavior_data.policy)
-        # print("First trajectory",behavior_data.trajectories[1])
         self.theta_b = np.asarray([0.01, -0.01, 1, 1])
-        self.delta = 0.05
+        self.delta = 0.02
         self.candidate_pdis = 0
-        self.safety_test_size = 200000 - data_partition
+        self.safety_test_size = int(data_partition * 0.4)
+        print("safety_data", self.safety_test_size)
         self.safety_pdis = 0
-        # self.pdis_for_data = 0
-        self.c = 1.028285376929881
+        self.c = 2*1.028285376929881
         self.candidate_pdis_history = np.array([])
         self.safety_pdis_history = np.array([])
-        # self.pdis_history = np.array([])
         self.linear_softmax = LinearSoftmax(self.behavior_data.num_action, self.behavior_data.fourier_order,
                                             self.behavior_data.num_states,
                                             )
 
-    def calculate_PDIS_per_episode(self, behavior_history, theta_e):
-        # behavior_history = self.behavior_data.trajectories[1]
+    def calculate_pi_b(self):
+        behavior_policy = [[]]
+        print("Pi_b is being calculated")
+        for trajectory in self.behavior_data.trajectories[1:self.data_partition + self.safety_test_size + 1]:
+            pi_b = []
+            probabilities = 1
+            for i in range(len(trajectory) // 3):
+                state = float(trajectory[i * 3])
+                action = int(trajectory[i * 3 + 1])
+                action_probability = self.linear_softmax.linear_softmax_probs(self.theta_b, state, action)
+                probabilities = probabilities * action_probability
+                pi_b.append(probabilities)
+            behavior_policy.append(pi_b)
+            # print("For trajectory", trajectory, ":pi_b is: ", pi_b)
+        self.pi_b = np.array(behavior_policy[1:])
+        print("pi_b is calculated")
+        # print("size of pi_b:", len(self.pi_b))
+
+    def calculate_PDIS_per_episode(self, behavior_history, theta_e, pi_b):
         pi_e = 1
-        pi_b = 1
         pdis = 0
-        policy_representation = []
+        # print("pi_b", pi_b)
         for i in range(len(behavior_history) // 3):
             state = float(behavior_history[i * 3])
             action = int(behavior_history[i * 3 + 1])
             reward = float(behavior_history[i * 3 + 2])
-            action_probabilities_b = self.linear_softmax.linear_softmax_probs(self.theta_b, state, action)
             action_probabilities_e = self.linear_softmax.linear_softmax_probs(theta_e, state, action)
-            pi_b = pi_b * action_probabilities_b
             pi_e = pi_e * action_probabilities_e
-            pdis = pdis + (pi_e / pi_b) * reward
-            policy_representation.append(action_probabilities_b)
-        # print("PDIS:", pdis)
-        # print("policy_representation:", policy_representation)
+            pdis = pdis + (pi_e / pi_b[i]) * reward
         return pdis
 
     def generate_pdis_for_candidate(self, theta_e):
         pdis_history = []
+        # self.calculate_pi_b()
+        # print(self.pi_b)
         count = 0
-        for history in self.behavior_data.trajectories[1:self.data_partition]:
-            # print("history", history)
-            pdis_per_episdode = self.calculate_PDIS_per_episode(history, theta_e)
-            # print(pdis_per_episdode)
-            # np.append(self.candidate_pdis_history,pdis_per_episdode)
+        for history in self.behavior_data.trajectories[1:self.data_partition + 1]:
+            pdis_per_episdode = self.calculate_PDIS_per_episode(history, theta_e, self.pi_b[count])
             pdis_history.append(pdis_per_episdode)
-            # count += 1
-            # if(count%5000==0):
-            #     print("pdis_per_episdode",pdis_per_episdode)
+            # print("in Candidate for trajectory:", history, "pi_b:", self.pi_b[count])
+            count += 1
         self.candidate_pdis_history = np.array(pdis_history)
+        # print("candidate_data_processed:", count + 1)
+        # print("candidate_pdis_len", len(self.candidate_pdis_history))
 
     def estimate_J_theta_candidate(self, theta_e):
-        # print("theta_e",theta_e)
         self.generate_pdis_for_candidate(theta_e)
-        # print("History of J",self.candidate_pdis_history)
-        # print("Max PDIS",np.max(self.pdis_history))
         self.candidate_pdis = np.mean(self.candidate_pdis_history)
-        # print("J_e:", self.pdis_for_data)
-        # return self.pdis_for_data
         return self.candidate_pdis
 
     def upper_bound(self, theta_e):
-        # safety_test_size = 200000 - self.data_partition
-        safety_test_size = np.sqrt(self.safety_test_size)
-        upper_limit = self.estimate_J_theta_candidate(theta_e) - 2 * (
-            np.divide(self.standard_deviation_pdis(), safety_test_size)) * stats.t.ppf(1 - self.delta,
-                                                                                       safety_test_size - 1)
-        print("Upper_Limit is", upper_limit)
+        safety_test_size = self.safety_test_size
+        upper_limit = self.estimate_J_theta_candidate(theta_e) - (2 * (
+            np.divide(self.standard_deviation_pdis(), np.sqrt(safety_test_size))) * stats.t.ppf(1 - self.delta,
+                                                                                                safety_test_size - 1))
         if upper_limit >= self.c:
-            return upper_limit
+            return self.candidate_pdis
         else:
-            return -100000
-
-    # def student_t_test(self):
-    #     # print("J_Theta", self.pdis_for_data)
-    #     return self.candidate_pdis - (
-    #             self.standard_deviation_pdis() / np.sqrt(len(self.candidate_pdis_history)) * stats.t.ppf(
-    #         1 - self.delta, len(self.candidate_pdis_history) - 1))
+            return -100000+upper_limit
 
     def standard_deviation_pdis(self):
-        pdis_list = self.candidate_pdis_history
-        pdis_for_data = self.candidate_pdis
-        return np.sqrt(np.sum((pdis_list - pdis_for_data) ** 2) / (len(pdis_list) - 1))
-
-    #
+        return np.std(self.candidate_pdis_history)
+        ##pdis_candidate_list = self.candidate_pdis_history
+        # pdis_for_candidate_data = self.candidate_pdis
+        # return np.sqrt(np.sum((pdis_candidate_list - pdis_for_candidate_data) ** 2) / (len(pdis_candidate_list) - 1))
 
     def generate_pdis_for_safety(self, theta_e):
         # print("Theta_e for safety", theta_e)
         pdis_history_safety = []
-        start = self.data_partition
-        end = self.data_partition + self.safety_test_size
-        # count = 1
-        # print(start,  end)
+        start = self.data_partition + 1
+        end = self.data_partition + self.safety_test_size + 1
+        count = start
         for history in self.behavior_data.trajectories[start:end]:
-            # print("history", history, "End",end)
-            pdis_per_episdode = self.calculate_PDIS_per_episode(history, theta_e)
-            #print("pdis_per_episdode",pdis_per_episdode)
-            # np.append(self.candidate_pdis_history,pdis_per_episdode)
+            # print("count is:", count)
+            # print("In safety for trajectory",history,"pi_b is",self.pi_b[count-1])
+            pdis_per_episdode = self.calculate_PDIS_per_episode(history, theta_e, self.pi_b[count - 1])
             pdis_history_safety.append(pdis_per_episdode)
-            # count += 1
-            # if(count%5000==0):
-            #     print("pdis_per_episdode",pdis_per_episdode)
-        # print(count)
+            count += 1
         self.safety_pdis_history = np.array(pdis_history_safety)
+        # print("length of safety pdis", len(self.safety_pdis_history))
 
     def estimate_J_theta_safety(self, theta_e):
         self.generate_pdis_for_safety(theta_e)
-        # print("safety_pdis_history",self.safety_pdis_history)
         self.safety_pdis = np.mean(self.safety_pdis_history)
         return self.safety_pdis
 
     def standard_deviation_safety(self):
-        pdis_list = self.safety_pdis_history
-        pdis_for_safety = self.safety_pdis
-        return np.sqrt(np.sum((pdis_list - pdis_for_safety) ** 2) / (len(pdis_list) - 1))
+        return np.std(self.safety_pdis_history)
+        # pdis_list = self.safety_pdis_history
+        # pdis_for_safety = self.safety_pdis
+        # return np.sqrt(np.sum((pdis_list - pdis_for_safety) ** 2) / (len(pdis_list) - 1))
 
-    def execute_safety_test(self, theta_e):
-        print("safety theta",theta_e)
+    def execute_safety_test(self, theta_e,i):
+        # print("safety theta", theta_e)
+        print("Executing safety test")
         self.estimate_J_theta_safety(theta_e)
         b = self.safety_pdis - (
                 np.divide(self.standard_deviation_safety(), np.sqrt(self.safety_test_size)) * stats.t.ppf(
             1 - self.delta, self.safety_test_size - 1))
+
+        print("B", b)
+        print("safety_test_estimate:", self.safety_pdis)
         if b >= self.c:
             print("safety test passed")
+            np.savetxt("policy"+str(i)+".csv", theta_e)
         else:
             print("safety test failed")
 
-pdis = PDIS("data.csv", 50000)
-print("Student_T_Test", pdis.estimate_J_theta_candidate([1.30716094, -4.01400105, -2.39934448,  3.18802846]))
+    @pi_b.setter
+    def pi_b(self, value):
+        self._pi_b = value
+
+
+# pdis = PDIS("data.csv", 200000)
+# pdis.calculate_pi_b()
+# print("Student_T_Test", pdis.estimate_J_theta_candidate([-4.52376687, 2.71429282, -9.23526859, 8.41318765]))
+# print("safet_test", pdis.execute_safety_test([2.06836668, -1.40374511, -2.26690564, -0.70637846]))
+# print("Student_T_Test", pdis.estimate_J_theta_candidate([1.30716094, -4.01400105, -2.39934448,  3.18802846]))
 
 # print("Student_T_Test", pdis.estimate_J_theta_candidate([-162.42145502, 568.02024562, -625.48083063, -30.46094259]))
 
